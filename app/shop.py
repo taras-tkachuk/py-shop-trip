b593f1e0e2a603c3d88f04b66e3a4cbc269cf42f
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-06-01 03:04:08 +0300

    .

--- app/shop.py
+++ app/shop.py
@@ -22,9 +22,9 @@ class Shop:
 
     def print_receipt(self, customer: Customer) -> None:
         print(f"Date: "
-              f"{datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n"
-              f"Thanks, {customer.name}, for your purchase!\n"
-              "You have bought:")
+              f"{datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}")
+        print(f"Thanks, {customer.name}, for your purchase!")
+        print("You have bought:")
         for product, amount in customer.product_cart.items():
             price = self.products[product] * amount
             print(
